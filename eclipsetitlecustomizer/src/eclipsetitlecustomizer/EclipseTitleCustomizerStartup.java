f36fe3985ab76f662be41db0f78399160c5e8b3e
devivar <devivar@xhddevivar30.xlnx.xilinx.com>
2013-10-29 14:57:16 +0530

    Fixed npe

--- eclipsetitlecustomizer/src/eclipsetitlecustomizer/EclipseTitleCustomizerStartup.java
+++ eclipsetitlecustomizer/src/eclipsetitlecustomizer/EclipseTitleCustomizerStartup.java
@@ -52,7 +52,10 @@ public class EclipseTitleCustomizerStartup implements IStartup {
 		Display.getDefault().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().setText(customizedName);
+				IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				if (workbenchWindow != null) {
+					workbenchWindow.getShell().setText(customizedName);
+				}
 			}
 		});
 	}
